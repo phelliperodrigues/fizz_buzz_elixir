b652ff19e5eb2cd011568169f7cb2e97fc34155d
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2022-08-03 18:04:48 -0300

    :heavy_check_mark: Create test from FizzBuzz Module

--- test/fizz_buzz_test.exs
+++ test/fizz_buzz_test.exs
@@ -1,8 +1,15 @@
 defmodule FizzBuzzTest do
   use ExUnit.Case
-  doctest FizzBuzz
 
-  test "greets the world" do
-    assert FizzBuzz.hello() == :world
+  describe "build/1" do
+    test "when a valid file is provided, returns the converted list" do
+      exprected_response = {:ok, [1, 2, :fizz, 4, :buzz, :buzz, :fizzbuzz, :buzz]}
+      assert FizzBuzz.build("numbers.txt") == exprected_response
+    end
+
+    test "when an invalid file is provided, returns an error " do
+      exprected_response = {:error, "Error reading the file: enoent"}
+      assert FizzBuzz.build("invalid.txt") == exprected_response
+    end
   end
 end
